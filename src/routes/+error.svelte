57baa51ed4df2aa74bb331f51493b230654a56b5
The-SamL <samuel.labagnere@gmail.com>
2024-04-09 19:39:28 +0200

    :lipstick: New error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { page } from '$app/stores';
+    console.log($page?.error);
+</script>
+
+<section class="flex flex-col items-center justify-center w-screen h-screen">
+    <h1 class="text-4xl font-bold">{$page?.error?.message}</h1>
+    <p class="italic">Oups, il semblerait que vous ayez rencontré une erreur</p>
+
+    <a href="/" class="btn-link my-5">&lt; Accueil</a>
+</section>
